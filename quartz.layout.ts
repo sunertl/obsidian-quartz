81eb349c8918c311a7cc76ae7e067275277d2ebd
Stefan Unertl <121638820+sunertl@users.noreply.github.com>
2024-03-13 18:48:27 -0700

    Quartz sync: Mar 13, 2024, 6:48 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,20 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
+  Component.Explorer({
+    title: "Explorer", // title of the explorer component
+    folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+    folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
+    useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
+    // Sort order: folders first, then files. Sort folders and files alphabetically
+    sortFn: (a, b) => {
+      ... // default implementation shown later
+    },
+    filterFn: filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
+    mapFn: undefined,
+    // what order to apply functions in
+    order: ["filter", "map", "sort"],
+  })
   footer: Component.Footer({
     links: {
       "Bluesky": "https://bsky.app/profile/carygrantler.bsky.social",
