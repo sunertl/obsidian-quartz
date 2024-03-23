3aef3162f1b00b3f8aef7957fc50fad654c9c190
Stefan Unertl <121638820+sunertl@users.noreply.github.com>
2024-03-22 17:35:32 -0700

    Quartz sync: Mar 22, 2024, 5:35 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,7 +16,7 @@ export default ((opts?: Options) => {
         <hr />
         <p>
         {i18n(cfg.locale).components.footer.createdWith}{" "}
-        <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> by Stefan Unertk © {year}"
+        <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> & filled with content by Stefan Unertl © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
